26dbd290fe471779fd6f68ab469038be443be2fb
YamiKawa1 <jehanzao@gmail.com>
2022-05-04 11:59:19 -0400

    a

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ import { createGenre } from './models/Genero';
 app.listen(2000, async () => {
     // Cambiar esto una vez deje de haber cambios en la base de datos 
     // await sequelize.sync()
-    await sequelize.sync({force: true})
+    await sequelize.sync({alter: true})
     await createGenre()
     console.log('listening on port', 2000);
 })
